9584d0568b3b68d849f9cf3718c37e196a909e14
EfimBulien <isip_e.m.bulien@mpt.ru>
2024-03-18 13:33:41 +0300

    first commit

---
+++ DriverClass.cs
@@ -0,0 +1,29 @@
+﻿namespace lab1ef
+{
+    internal class DriverClass
+    {
+        public int DriverID {  get; private set; }
+        public string DriverFirstname { get; private set; }
+        public string DriverSurname { get; private set; }
+        public string DriverMiddlename { get; private set; }
+        public int CarID { get; private set; }
+        public string CarModel { get; private set; }
+        public string CarNumber { get; private set; }
+        public string CarColor { get; private set; }
+        public DriverClass(drivers driver) 
+        {
+            DriverID = driver.id_driver;
+            DriverFirstname = driver.firstname;
+            DriverSurname = driver.surname;
+            DriverMiddlename = driver.middlename;
+
+            if (driver.cars != null)
+            {
+                CarID = driver.cars.id_car;
+                CarModel = driver.cars.model;
+                CarNumber = driver.cars.number;
+                CarColor = driver.cars.color;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Windows;
+
+namespace lab1ef
+{
+    public partial class MainWindow : Window
+    {
+        private lab1Entities lab1Entities = new lab1Entities();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DriversData.ItemsSource = lab1Entities.drivers.ToList();
+        }
+    }
+}
+
